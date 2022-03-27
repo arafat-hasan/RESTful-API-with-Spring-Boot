4c428886f82c3dbe03087a337ccd933dc1efc943
Arafat Hasan <opendoor.arafat@gmail.com>
2022-03-27 17:12:57 +0600

    spring initializr

---
+++ src/main/java/com/arafathasan/RESTfulAPIwithSpringBoot/ResTfulApiWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.arafathasan.RESTfulAPIwithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulApiWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arafathasan/RESTfulAPIwithSpringBoot/ResTfulApiWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arafathasan.RESTfulAPIwithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
