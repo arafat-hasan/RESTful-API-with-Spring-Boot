20f0a8a4e59f7db41f73880544f5d5af2c75a2bb
Arafat Hasan <opendoor.arafat@gmail.com>
2022-03-30 14:31:43 +0600

    CRUD operations completed

--- src/main/java/com/arafathasan/RESTfulAPIwithSpringBoot/student/StudentController.java
+++ src/main/java/com/arafathasan/RESTfulAPIwithSpringBoot/student/StudentController.java
@@ -3,6 +3,7 @@ package com.arafathasan.RESTfulAPIwithSpringBoot.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -23,5 +24,18 @@ public class StudentController {
     @PostMapping
     public void registerNewStudent(@RequestBody Student student){
         studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long id){
+        studentService.deleteStudent(id);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email){
+        studentService.updateStudent(studentId, name, email);
     }
 }
